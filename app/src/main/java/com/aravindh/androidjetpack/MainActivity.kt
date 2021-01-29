a8fe99574d2325448aa32b63219fb310ca09114e
Aravindh <aravindh.samidurai@tagitmobile.com>
2021-01-29 18:35:21 +0530

    Added localazy library

--- app/src/main/java/com/aravindh/androidjetpack/MainActivity.kt
+++ app/src/main/java/com/aravindh/androidjetpack/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.aravindh.androidjetpack
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.localazy.android.Localazy
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        locale()
+    }
+
+    private fun locale() {
+        val currentLocale: Locale = Localazy.getCurrentLocale()
+        Log.d(TAG, "lang : ${currentLocale.language}")
+
+        Log.d(TAG, "isEnabled : ${Localazy.isEnabled()}")
+
+        val localeHi = Locale("ta")
+        Localazy.forceLocale(localeHi, false)
     }
 }
